34a060e783163d07ac286c1fa430cfe07658ee66
Luis <lvtamez@gmail.com>
2018-09-21 13:36:39 -0400

    week 3

---
+++ mvcmusicstore/Controllers/AlbumsController.cs
@@ -0,0 +1,26 @@
+﻿using mvcmusicstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcmusicstore.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        // GET: Albums
+        public ActionResult Index()
+        {
+            var albums = new List<Album>();
+            for(int i=1;i<10;i++)
+            {
+                albums.Add(new Album { Title = "Album " + i.ToString() });
+            }
+            //add albums to viewbag for display
+            //ViewBag.albums = albums;
+            //pass the strongly typed list instead
+            return View(albums);
+        }
+    }
+}
---
+++ mvcmusicstore/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcmusicstore.Models
+{
+    public class Album
+    {
+        //create mock list of type album
+        
+        public string Title { get; set; }
+    }
+}
